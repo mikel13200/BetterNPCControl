b6a1103ce20da2a8327abe2fe0b9cfd0b6fb66f3
Flayz <99353602+Frank187Dev@users.noreply.github.com>
2022-08-29 18:23:58 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,17 +5,16 @@ Config = {}
 Config.NPCTraffic = 0.3 -- How many NPCs cars should spawn? 0.0 - 1.0
 Config.NPCCitizens = 0.3 -- How many NPCs should be spawned on the walkway 0.0 - 1.0
 Config.ParkedCars = 0.3 -- How many parked NPC cars should be spawned? 0.0 - 1.0 
-Config.ScenarioPeds = 0.0 -- Like ambulance and cops 0.0 - 1.0
 
 --
 
-Config.disableCops = true 
+Config.disableCops = true -- true = allow cops to spawn
 
-Config.StealDrivingVehicle = false -- Prevent players from stealing driving NPC Vehicles
+Config.StealDrivingVehicle = false -- false = prevent players from stealing driving NPC Vehicles
 
-Config.DispatchService = false -- Disable all dispatches, such as NPC medics arriving to death scenes
+Config.DispatchService = false -- false = Disable all dispatches, such as NPC medics arriving to death scenes
 
-Config.NPCGangsAttack = false -- Disable NPC gangs attacking users. Especially useful for making the bike gang & purple gang not constantly angry
+Config.NPCGangsAttack = true -- Disable NPC gangs attacking users. Especially useful for making the bike gang & purple gang not constantly angry
 
 Config.GetFreeWeaponsWhenEnteringVehicle = false -- prevent users from getting free weapons when entering police vehicles & helicopters
 
