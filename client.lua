1a76bf7fa95640954ae19646ee04529bec740fb1
Flayz <99353602+Frank187Dev@users.noreply.github.com>
2022-08-29 18:23:29 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -57,17 +57,12 @@ end)
 local relationshipTypes = {GetHashKey('PLAYER'), GetHashKey('CIVMALE'), GetHashKey('CIVFEMALE'), GetHashKey('GANG_1'), GetHashKey('GANG_2'), GetHashKey('GANG_9'), GetHashKey('GANG_10'), GetHashKey('AMBIENT_GANG_LOST'), GetHashKey('AMBIENT_GANG_MEXICAN'), GetHashKey('AMBIENT_GANG_FAMILY'), GetHashKey('AMBIENT_GANG_BALLAS'), GetHashKey('AMBIENT_GANG_MARABUNTE'), GetHashKey('AMBIENT_GANG_CULT'), GetHashKey('AMBIENT_GANG_SALVA'), GetHashKey('AMBIENT_GANG_WEICHENG'), GetHashKey('AMBIENT_GANG_HILLBILLY'), GetHashKey('DEALER'), GetHashKey('COP'), GetHashKey('PRIVATE_SECURITY'), GetHashKey('SECURITY_GUARD'), GetHashKey('ARMY'), GetHashKey('MEDIC'), GetHashKey('FIREMAN'), GetHashKey('HATES_PLAYER'), GetHashKey('NO_RELATIONSHIP'), GetHashKey('SPECIAL'), GetHashKey('MISSION2'), GetHashKey('MISSION3'), GetHashKey('MISSION4'), GetHashKey('MISSION5'), GetHashKey('MISSION6'), GetHashKey('MISSION7'), GetHashKey('MISSION8')}
 
 Citizen.CreateThread(function()
-	if Config.NPCGangsAttack == false then
-    while true do
-        Citizen.Wait(2000)
+	if Config.NPCGangsAttack == true then
         local playerHash = GetHashKey('PLAYER')
-
-
 
         for k,groupHash in ipairs(relationshipTypes) do
             SetRelationshipBetweenGroups(1, playerHash, groupHash)
             SetRelationshipBetweenGroups(1, groupHash, playerHash)
-        end
 	end
     end
 end)
@@ -90,6 +85,7 @@ Citizen.CreateThread(function()
       RemoveAllPickupsOfType(0xDF711959) -- carbine rifle
       RemoveAllPickupsOfType(0xF9AFB48F) -- pistol
       RemoveAllPickupsOfType(0xA9355DCD) -- pumpshotgun
-    end
+		end
+      end
   end)
 
